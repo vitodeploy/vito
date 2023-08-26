dacbb10ef0631ff969284065014c43628ca1a4fe
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2023-08-26 09:59:36 +0200

    fix #12

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Enums\ServiceStatus;
+use App\Enums\SiteType;
+use App\Models\Server;
+use App\Models\Site;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -12,9 +16,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Test User',
             'email' => 'user@example.com',
+        ]);
+        $server = Server::factory()->create([
+            'user_id' => $user->id
+        ]);
+        $server->services()->create([
+            'type' => 'database',
+            'name' => config('core.databases_name.mysql80'),
+            'version' => config('core.databases_version.mysql80'),
+            'status' => ServiceStatus::READY
+        ]);
+        $server->services()->create([
+            'type' => 'php',
+            'type_data' => [
+                'extensions' => [],
+            ],
+            'name' => 'php',
+            'version' => '8.1',
+            'status' => ServiceStatus::READY
+        ]);
+        $server->services()->create([
+            'type' => 'webserver',
+            'name' => 'nginx',
+            'version' => 'latest',
+            'status' => ServiceStatus::READY
+        ]);
+        Site::factory()->create([
+            'server_id' => $server->id,
+            'type' => SiteType::LARAVEL
         ]);
     }
 }
--- resources/views/livewire/application/deploy.blade.php
+++ resources/views/livewire/application/deploy.blade.php
@@ -1,5 +1,5 @@
 <div>
-    @if($site->deploymentScript && $site->deploymentScript->content)
+    @if($site->deploymentScript?->content)
         <x-primary-button wire:click="deploy" wire:loading.attr="disabled">{{ __("Deploy") }}</x-primary-button>
     @endif
 </div>
