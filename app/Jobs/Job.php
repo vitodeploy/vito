38e23a1ceb31e0d0cd172a3a4287bcf44894e1ff
Saeed Vaziry <61919774+saeedvaziry@users.noreply.github.com>
2023-09-29 23:08:56 +0200

    fix consecutive deployment issue (#62)
    * fix consecutive deployment issue
    
    * fix styles

--- app/Jobs/Job.php
+++ app/Jobs/Job.php
@@ -3,13 +3,12 @@
 namespace App\Jobs;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-abstract class Job implements ShouldQueue, ShouldBeUnique
+abstract class Job implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 }
--- resources/commands/system/run-script.sh
+++ resources/commands/system/run-script.sh
@@ -2,6 +2,4 @@ if ! cd __path__; then
     echo 'VITO_SSH_ERROR' && exit 1
 fi
 
-if ! __script__; then
-    echo 'VITO_SSH_ERROR' && exit 1
-fi
+__script__
