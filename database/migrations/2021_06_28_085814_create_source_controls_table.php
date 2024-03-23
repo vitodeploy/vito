3afb6b7eb41384c51eff4b200162a6436914a528
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2024-03-23 17:39:51 +0100

    sqlite to mysql migrate command

--- database/migrations/2021_06_28_085814_create_source_controls_table.php
+++ database/migrations/2021_06_28_085814_create_source_controls_table.php
@@ -11,8 +11,8 @@ return new class extends Migration
         Schema::create('source_controls', function (Blueprint $table) {
             $table->id();
             $table->string('provider');
-            $table->json('provider_data')->nullable()->after('provider');
-            $table->longText('access_token')->nullable(); // @TODO: remove this column
+            $table->json('provider_data')->nullable();
+            $table->longText('access_token')->nullable();
             $table->timestamps();
         });
     }
