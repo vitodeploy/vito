8e60ac306324b1fd2e8485722599952c08c97038
saravana sai <saravanasai002@gmail.com>
2024-03-29 22:05:17 +0530

    Added footer & added version on config/app.php

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,11 @@
+<footer
+    class="rounded-lg bg-white p-8 antialiased shadow dark:bg-gray-800 sm:flex sm:items-center sm:justify-between sm:p-6 xl:p-8">
+    <p class="mb-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:mb-0">
+        &copy; 2024
+        <a href="https://vitodeploy.com/" class="hover:underline" target="_blank">Vitodeploy</a>
+        . All rights reserved.
+    </p>
+    <div class="flex items-center justify-center space-x-1">
+        <div>{{ config('app.version') }}</div>
+    </div>
+</footer>
