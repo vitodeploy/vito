cca4ab7ae338b7e78b6c0c58ab4924180e38ee7e
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2024-03-29 18:40:20 +0100

    fix code editor

--- resources/views/components/code-editor.blade.php
+++ resources/views/components/code-editor.blade.php
@@ -11,27 +11,31 @@
         disabled: @js($disabled),
         lang: @js($lang),
         init() {
-            let editor = null
-            let theme =
-                document.documentElement.className === 'dark'
-                    ? 'one-dark'
-                    : 'github'
-            editor = window.ace.edit(this.editorId, {})
-            let contentElement = document.getElementById(`text-${this.editorId}`)
-            editor.setValue(contentElement.innerText, 1)
-            if (this.disabled) {
-                editor.setReadOnly(true)
-            }
-            editor.getSession().setMode(`ace/mode/${this.lang}`)
-            editor.setTheme(`ace/theme/${theme}`)
-            editor.setFontSize('15px')
-            editor.setShowPrintMargin(false)
-            editor.on('change', () => {
-                contentElement.innerHTML = editor.getValue()
-            })
-            document.body.addEventListener('color-scheme-changed', (event) => {
-                theme = event.detail.theme === 'dark' ? 'one-dark' : 'github'
+            document.body.addEventListener('htmx:afterSettle', (event) => {
+                let editor = null
+                let theme =
+                    document.documentElement.className === 'dark'
+                        ? 'one-dark'
+                        : 'github'
+                editor = window.ace.edit(this.editorId, {})
+                let contentElement = document.getElementById(
+                    `text-${this.editorId}`,
+                )
+                editor.setValue(contentElement.innerText, 1)
+                if (this.disabled) {
+                    editor.setReadOnly(true)
+                }
+                editor.getSession().setMode(`ace/mode/${this.lang}`)
                 editor.setTheme(`ace/theme/${theme}`)
+                editor.setFontSize('15px')
+                editor.setShowPrintMargin(false)
+                editor.on('change', () => {
+                    contentElement.innerHTML = editor.getValue()
+                })
+                document.body.addEventListener('color-scheme-changed', (event) => {
+                    theme = event.detail.theme === 'dark' ? 'one-dark' : 'github'
+                    editor.setTheme(`ace/theme/${theme}`)
+                })
             })
         },
     }"
