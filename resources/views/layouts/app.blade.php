57515a84486bc8c263ed45822f871c0836467cfc
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2024-04-03 21:50:49 +0200

    use textarea for code editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,8 +21,6 @@
         <link rel="preconnect" href="https://fonts.bunny.net" />
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <script src="{{ asset("static/libs/ace/ace.js") }}"></script>
-
         @include("layouts.partials.favicon")
 
         <!-- Scripts -->
