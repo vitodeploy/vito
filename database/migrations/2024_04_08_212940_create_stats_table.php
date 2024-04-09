81c85727261b0a95c4e0796b672d8b72c68c973e
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2024-04-09 19:26:01 +0200

    wip

---
+++ database/migrations/2024_04_08_212940_create_stats_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stats', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('server_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stats');
+    }
+};
