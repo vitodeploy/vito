c0f78daa96ac4c244c1b8ee76feb277dca7d56da
Borja Jiménez <truestylegs@gmail.com>
2024-04-11 22:10:41 +0200

    change button, to fit with requeriments

--- resources/views/application/deploy.blade.php
+++ resources/views/application/deploy.blade.php
@@ -6,8 +6,8 @@
             hx-swap="outerHTML"
             hx-select="#deploy"
         >
-            <x-primary-button class="flex items-center justify-between gap-1" :active="true" hx-disable>
-                {{ __("Deploy") }} <x-heroicon name="o-play-circle" class="h-5 w-5" />
+            <x-primary-button class="flex items-center justify-between" :active="true" hx-disable>
+                {{ __("Deploy") }} <x-heroicon name="o-play-circle" class="ml-1 h-5 w-5" />
             </x-primary-button>
         </form>
     @endif
