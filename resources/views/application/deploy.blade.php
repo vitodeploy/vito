a2ed06fb74ac3c958ee46ecf08a2fa4861ba813c
Borja Jiménez <truestylegs@gmail.com>
2024-04-14 11:50:44 +0200

    fix pretier errors

--- resources/views/application/deploy.blade.php
+++ resources/views/application/deploy.blade.php
@@ -7,7 +7,8 @@
             hx-select="#deploy"
         >
             <x-primary-button class="flex items-center justify-between" :active="true" hx-disable>
-                {{ __("Deploy") }} <x-heroicon name="o-play-circle" class="ml-1 h-5 w-5" />
+                {{ __("Deploy") }}
+                <x-heroicon name="o-play-circle" class="ml-1 h-5 w-5" />
             </x-primary-button>
         </form>
     @endif
