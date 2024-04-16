8ea2229a22c469b15a4b48078593aaef89d0932f
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2024-04-16 23:18:54 +0200

    enable scheduler

--- app/Console/Commands/GetMetricsCommand.php
+++ app/Console/Commands/GetMetricsCommand.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Models\Server;
 use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Builder;
 
 class GetMetricsCommand extends Command
 {
@@ -13,7 +14,10 @@ class GetMetricsCommand extends Command
 
     public function handle(): void
     {
-        Server::query()->chunk(10, function ($servers) {
+        Server::query()->whereHas('services', function (Builder $query) {
+            $query->where('type', 'monitoring')
+                ->where('name', 'remote-monitor');
+        })->chunk(10, function ($servers) {
             /** @var Server $server */
             foreach ($servers as $server) {
                 $info = $server->os()->resourceInfo();
