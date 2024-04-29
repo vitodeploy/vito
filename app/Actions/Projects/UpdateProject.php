5debbd4f5d12acd16ad13364ba6c06cc90d048bb
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2024-04-29 21:11:07 +0200

    update project lowercase

--- app/Actions/Projects/UpdateProject.php
+++ app/Actions/Projects/UpdateProject.php
@@ -10,6 +10,10 @@ class UpdateProject
 {
     public function update(Project $project, array $input): Project
     {
+        if (isset($input['name'])) {
+            $input['name'] = strtolower($input['name']);
+        }
+
         $this->validate($project, $input);
 
         $project->name = $input['name'];
