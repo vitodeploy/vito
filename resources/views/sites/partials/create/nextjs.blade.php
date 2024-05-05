920f67935bfb391415dd3f7771b650ad4c424fc2
Mehrdad <mehrdad.motaghifar@gmail.com>
2024-05-06 02:43:55 +0330

    lint:fix for nextjs.blade.php

--- resources/views/sites/partials/create/nextjs.blade.php
+++ resources/views/sites/partials/create/nextjs.blade.php
@@ -1,7 +1,7 @@
-@include('sites.partials.create.fields.web-directory')
+@include("sites.partials.create.fields.web-directory")
 
-@include('sites.partials.create.fields.source-control')
+@include("sites.partials.create.fields.source-control")
 
-@include('sites.partials.create.fields.repository')
+@include("sites.partials.create.fields.repository")
 
-@include('sites.partials.create.fields.branch')
+@include("sites.partials.create.fields.branch")
