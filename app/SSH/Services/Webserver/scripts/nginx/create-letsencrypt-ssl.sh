cce851c2015afbd86ee6baa4060f926de6c36f44
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2024-05-13 00:34:54 +0200

    fix letsencrypt for aliases

--- app/SSH/Services/Webserver/scripts/nginx/create-letsencrypt-ssl.sh
+++ app/SSH/Services/Webserver/scripts/nginx/create-letsencrypt-ssl.sh
@@ -1,3 +1,3 @@
-if ! sudo certbot certonly --force-renewal --nginx --noninteractive --agree-tos --cert-name __domain__ -m __email__ -d __domain__ --verbose; then
+if ! sudo certbot certonly --force-renewal --nginx --noninteractive --agree-tos --cert-name __domain__ -m __email__ __domains__ --verbose; then
     echo 'VITO_SSH_ERROR' && exit 1
 fi
