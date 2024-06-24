3d17fb8d5b0b68080dd269e27926ded6c46a20b4
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2024-06-24 22:18:14 +0200

    fix #209

--- resources/views/components/console-view.blade.php
+++ resources/views/components/console-view.blade.php
@@ -1,5 +1,5 @@
 <div
-    {{ $attributes->merge(["class" => "relative h-[500px] w-full overflow-auto whitespace-pre-line rounded-md border border-gray-200 bg-black p-5 text-gray-50 dark:border-gray-700"]) }}
+    {{ $attributes->merge(["class" => "font-mono whitespace-pre relative h-[500px] w-full overflow-auto whitespace-pre-line rounded-md border border-gray-200 bg-black p-5 text-gray-50 dark:border-gray-700"]) }}
 >
     {{ $slot }}
 </div>
