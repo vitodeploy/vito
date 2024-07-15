e08bbbf9d4a3b92e5cd7d9c35c834643f9213071
klc <m_kilic@outlook.com.tr>
2024-07-15 10:24:41 +0300

    fixed issue using variable in .env

--- app/Actions/Site/UpdateEnv.php
+++ app/Actions/Site/UpdateEnv.php
@@ -9,6 +9,7 @@ class UpdateEnv
     public function update(Site $site, array $input): void
     {
         $env = str_replace('"', '\"', $input['env']);
+        $env = str_replace('${', '\${', $env);
 
         $site->server->os()->editFile(
             $site->path.'/.env',
