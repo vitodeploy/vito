bb094501fd202dbb89185d0a7ef3cee6a757a266
Ngô Quốc Đạt <hi@ngoquocdat.dev>
2024-10-03 23:51:55 +0700

    chore: delete `composer-setup.php` file after install completed in install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -103,6 +103,7 @@ service php${V_PHP_VERSION}-fpm restart
 # composer
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "unlink('composer-setup.php');"
 
 # setup website
 export COMPOSER_ALLOW_SUPERUSER=1
