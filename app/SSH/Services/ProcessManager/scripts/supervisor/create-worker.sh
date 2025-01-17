6ee76894f5543130a763c4f263a8c7957be5d027
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-01-18 00:11:26 +0100

    fix queues for isolated users

--- app/SSH/Services/ProcessManager/scripts/supervisor/create-worker.sh
+++ app/SSH/Services/ProcessManager/scripts/supervisor/create-worker.sh
@@ -1,6 +1,14 @@
-mkdir -p "$(dirname __log_file__)"
+if ! sudo mkdir -p "$(dirname __log_file__)"; then
+    echo 'VITO_SSH_ERROR' && exit 1
+fi
 
-touch __log_file__
+if ! sudo touch __log_file__; then
+    echo 'VITO_SSH_ERROR' && exit 1
+fi
+
+if ! sudo chown __user__:__user__ __log_file__; then
+    echo 'VITO_SSH_ERROR' && exit 1
+fi
 
 if ! sudo supervisorctl reread; then
     echo 'VITO_SSH_ERROR' && exit 1
