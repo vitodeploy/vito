b62055030303fc3b2b4dde483fdcc6f2e666df79
Pierlo <pierlooqup@gmail.com>
2025-01-29 08:07:40 +0800

    🍻🍻

--- tests/Feature/DeploymentCompletedTest.php
+++ tests/Feature/DeploymentCompletedTest.php
@@ -10,55 +10,55 @@ use Tests\TestCase;
 
 class DeploymentCompletedTest extends TestCase
 {
-    public function testRawText()
+    public function test_raw_text()
     {
         $deployment = new Deployment(['status' => 'finished']);
         $site = new Site(['domain' => 'example.com']);
         $notification = new DeploymentCompleted($deployment, $site);
 
-        $expectedText = "Deployment for site [example.com] has completed with status: finished";
+        $expectedText = 'Deployment for site [example.com] has completed with status: finished';
         $this->assertEquals($expectedText, $notification->rawText());
     }
 
-    public function testToEmail()
+    public function test_to_email()
     {
         $deployment = new Deployment(['status' => 'finished']);
         $site = new Site(['domain' => 'example.com']);
         $notification = new DeploymentCompleted($deployment, $site);
 
-        $mailMessage = $notification->toEmail(new \stdClass());
+        $mailMessage = $notification->toEmail(new \stdClass);
         $this->assertInstanceOf(MailMessage::class, $mailMessage);
         $this->assertEquals('Deployment Completed', $mailMessage->subject);
         $this->assertStringContainsString('Deployment for site [example.com] has completed with status: finished', $mailMessage->introLines[0]);
     }
 
-    public function testToSlack()
+    public function test_to_slack()
     {
         $deployment = new Deployment(['status' => 'finished']);
         $site = new Site(['domain' => 'example.com']);
         $notification = new DeploymentCompleted($deployment, $site);
 
-        $expectedText = "Deployment for site [example.com] has completed with status: finished";
-        $this->assertEquals($expectedText, $notification->toSlack(new \stdClass()));
+        $expectedText = 'Deployment for site [example.com] has completed with status: finished';
+        $this->assertEquals($expectedText, $notification->toSlack(new \stdClass));
     }
 
-    public function testToDiscord()
+    public function test_to_discord()
     {
         $deployment = new Deployment(['status' => 'finished']);
         $site = new Site(['domain' => 'example.com']);
         $notification = new DeploymentCompleted($deployment, $site);
 
-        $expectedText = "Deployment for site [example.com] has completed with status: finished";
-        $this->assertEquals($expectedText, $notification->toDiscord(new \stdClass()));
+        $expectedText = 'Deployment for site [example.com] has completed with status: finished';
+        $this->assertEquals($expectedText, $notification->toDiscord(new \stdClass));
     }
 
-    public function testToTelegram()
+    public function test_to_telegram()
     {
         $deployment = new Deployment(['status' => 'finished']);
         $site = new Site(['domain' => 'example.com']);
         $notification = new DeploymentCompleted($deployment, $site);
 
-        $expectedText = "Deployment for site [example.com] has completed with status: finished";
-        $this->assertEquals($expectedText, $notification->toTelegram(new \stdClass()));
+        $expectedText = 'Deployment for site [example.com] has completed with status: finished';
+        $this->assertEquals($expectedText, $notification->toTelegram(new \stdClass));
     }
 }
