7b96f5b6b1b6da06e9fdad9789146b3674760c9f
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-06-13 14:06:23 +0200

    phpstan

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Site;
-use App\SiteFeatures\LaravelOctane\Enable;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Spatie\RouteAttributes\Attributes\Get;
 
 class HomeController extends Controller
@@ -18,15 +15,5 @@ class HomeController extends Controller
         }
 
         return redirect()->route('login');
-    }
-
-    #[Get('/test')]
-    public function test(Request $request): string
-    {
-        $site = Site::query()->first();
-
-        new Enable($site)->handle($request);
-
-        return 'Laravel Octane enabled for site: '.$site->name;
     }
 }
