171105c55a35e7c14988be88889cb23383e072f8
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-08-02 18:50:57 +0200

    fix cron is missing on some providers

--- resources/views/ssh/os/install-dependencies.blade.php
+++ resources/views/ssh/os/install-dependencies.blade.php
@@ -1,4 +1,4 @@
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y software-properties-common curl zip unzip git gcc openssl ufw
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y software-properties-common curl zip unzip git gcc openssl ufw cron
 git config --global user.email "{{ $email }}"
 git config --global user.name "{{ $name }}"
 
