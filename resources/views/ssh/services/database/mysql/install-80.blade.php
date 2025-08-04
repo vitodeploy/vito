0ddd2304f81d6f487081c541ef815732468e1197
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-08-04 22:55:17 +0200

    fix mysql 8.0 installation

--- resources/views/ssh/services/database/mysql/install-80.blade.php
+++ resources/views/ssh/services/database/mysql/install-80.blade.php
@@ -1,12 +1,20 @@
+#!/bin/bash
 sudo DEBIAN_FRONTEND=noninteractive apt-get update
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install mysql-server -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y wget lsb-release gnupg
+
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.34-1_all.deb
+
+echo "mysql-apt-config mysql-apt-config/select-server select mysql-8.0" | sudo debconf-set-selections
+sudo DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.34-1_all.deb
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get update
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
 
 sudo systemctl unmask mysql.service
-
-sudo service mysql enable
-
-sudo service mysql start
+sudo systemctl enable mysql
+sudo systemctl start mysql
 
 if ! sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH auth_socket;"; then
     echo 'VITO_SSH_ERROR' && exit 1
