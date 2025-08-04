f8b941f8b2c45de8947c36b90d7fcd27e4833a70
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-08-04 17:01:27 +0200

    fix time format

--- resources/js/components/date-time.tsx
+++ resources/js/components/date-time.tsx
@@ -1,6 +1,6 @@
 import moment from 'moment';
 
-export default function DateTime({ date, format = 'YYYY-MM-DD hh:mm:ss', className }: { date: string; format?: string; className?: string }) {
+export default function DateTime({ date, format = 'YYYY-MM-DD HH:mm:ss', className }: { date: string; format?: string; className?: string }) {
   return (
     <time dateTime={date} className={className}>
       {moment(date).format(format)}
