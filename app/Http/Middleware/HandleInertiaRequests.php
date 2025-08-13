a93ad3e389daa52ebbb0138a1776a0789e4058ec
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-08-13 11:22:49 +0200

    fix phpstan

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -52,7 +52,7 @@ class HandleInertiaRequests extends Middleware
         // servers
         $servers = [];
         if ($user && $user->currentProject && $user->can('viewAny', [Server::class, $user->currentProject])) {
-            $servers = ServerResource::collection($user->currentProject?->servers);
+            $servers = ServerResource::collection($user->currentProject->servers);
         }
 
         $data = [];
