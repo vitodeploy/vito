91daa3482defa27804cbc33fc5a89a12c4068742
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-08-22 20:04:30 +0200

    improve server connection/disconnection notifications

--- app/Actions/Server/CheckConnection.php
+++ app/Actions/Server/CheckConnection.php
@@ -5,30 +5,36 @@ namespace App\Actions\Server;
 use App\Enums\ServerStatus;
 use App\Facades\Notifier;
 use App\Models\Server;
+use App\Notifications\ServerConnected;
 use App\Notifications\ServerDisconnected;
 use Throwable;
 
 class CheckConnection
 {
-    public function check(Server $server, int $retry = 1): Server
+    public function check(Server $server, int $retry = 2): Server
     {
-        $status = $server->status;
+        $status = $server->refresh()->status;
         try {
             $server->ssh()->connect();
             $server->refresh();
             if (in_array($status, [ServerStatus::DISCONNECTED, ServerStatus::UPDATING])) {
                 $server->status = ServerStatus::READY;
                 $server->save();
+                if ($status === ServerStatus::DISCONNECTED) {
+                    Notifier::send($server, new ServerConnected($server));
+                }
             }
         } catch (Throwable) {
             if ($retry > 0) {
-                sleep(2);
+                sleep(3);
 
                 return $this->check($server, $retry - 1);
             }
-            $server->status = ServerStatus::DISCONNECTED;
-            $server->save();
-            Notifier::send($server, new ServerDisconnected($server));
+            if ($status !== ServerStatus::DISCONNECTED) {
+                $server->status = ServerStatus::DISCONNECTED;
+                $server->save();
+                Notifier::send($server, new ServerDisconnected($server));
+            }
         }
 
         return $server;
