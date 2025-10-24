0d7dfd2aa9553c0ad03a1220590dccfced5b5e32
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2025-10-24 18:42:34 +0200

    Update load-balancer.blade.php

--- resources/views/ssh/services/webserver/nginx/vhost-blocks/load-balancer.blade.php
+++ resources/views/ssh/services/webserver/nginx/vhost-blocks/load-balancer.blade.php
@@ -11,7 +11,7 @@ location / {
     proxy_set_header SERVER_PORT $server_port;
     proxy_set_header REMOTE_ADDR $remote_addr;
     proxy_set_header Upgrade $http_upgrade;
-    proxy_set_header Connection "Upgrade";
+    proxy_set_header Connection $http_connection;
     proxy_set_header X-Real-IP $remote_addr;
     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     proxy_set_header X-Forwarded-Proto $scheme;
