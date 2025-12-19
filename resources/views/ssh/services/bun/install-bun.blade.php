220cb50cfc88cf889b5c449d9dbf3cf890e5403c
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2025-12-19 10:25:19 +0100

    Fix: move bunx binary to /usr/local/bin and update permissions for global access

--- resources/views/ssh/services/bun/install-bun.blade.php
+++ resources/views/ssh/services/bun/install-bun.blade.php
@@ -4,9 +4,11 @@ BUN_VERSION={{ $version }}
 # Install Bun using the official installer
 curl -fsSL https://bun.sh/install | bash -s "bun-v${BUN_VERSION}"
 
-# Move Bun binary to /usr/local/bin for global access
+# Move Bun and Bunx binaries to /usr/local/bin for global access
 sudo mv ~/.bun/bin/bun /usr/local/bin/
 sudo chmod a+x /usr/local/bin/bun
+sudo mv ~/.bun/bin/bunx /usr/local/bin/
+sudo chmod a+x /usr/local/bin/bunx
 
 # The installation script adds  "~/.bun/bin" to $PATH in "~/.bashrc" automatically, we should remove it
 sed -i '/\.bun\/bin/d' "$HOME/.bashrc" 2>/dev/null || true
