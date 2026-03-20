0b4a7730e2107abb00bff389b7c69e5ce6d41bf9
Richard Anderson <richard@andermail.co.uk>
2026-03-20 15:24:13 +0000

    fix for resolution of dns

--- resources/views/ssh/dns/check-domain-resolution.blade.php
+++ resources/views/ssh/dns/check-domain-resolution.blade.php
@@ -1,10 +1,8 @@
-NS=$(dig +short NS {!! $domain !!} | head -1)
-if [ -z "$NS" ]; then
-  PARENT=$(echo {!! $domain !!} | sed 's/^[^.]*\.//')
-  NS=$(dig +short NS $PARENT | head -1)
-fi
-if [ -z "$NS" ]; then
-  NS="1.1.1.1"
-fi
-dig +short A {!! $domain !!} @$NS
-dig +short AAAA {!! $domain !!} @$NS
+RESOLVED=""
+for RESOLVER in 1.1.1.1 8.8.8.8 9.9.9.9; do
+  RESOLVED="$RESOLVED
+$(dig +short +time=3 +tries=2 A {!! $domain !!} @$RESOLVER 2>/dev/null || true)"
+  RESOLVED="$RESOLVED
+$(dig +short +time=3 +tries=2 AAAA {!! $domain !!} @$RESOLVER 2>/dev/null || true)"
+done
+echo "$RESOLVED" | grep -v '^$' | sort -u
