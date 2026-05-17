125d49ee12232302b8e07dad0e458637c6acce19
Richard Anderson <richard@andermail.co.uk>
2026-05-17 16:12:05 +0100

    fix dropbox

--- resources/views/ssh/storage/dropbox/upload.blade.php
+++ resources/views/ssh/storage/dropbox/upload.blade.php
@@ -3,4 +3,4 @@ curl -sb --location --request POST 'https://content.dropboxapi.com/2/files/uploa
 --header 'Dropbox-API-Arg: {"path":"{{ $dest }}"}' \
 --header 'Content-Type: text/plain; charset=dropbox-cors-hack' \
 --header 'Authorization: Bearer {{ $token }}' \
---data-binary '@{{ $src }}'
+--data-binary '{{ "@" . $src }}'
