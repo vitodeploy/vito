871e625b975c7937c1deb432207e3dbc893483b9
Richard Anderson <richard@andermail.co.uk>
2026-05-23 16:20:49 +0100

    more feedback

--- resources/views/ssh/services/webserver/caddy/default-vhost.blade.php
+++ resources/views/ssh/services/webserver/caddy/default-vhost.blade.php
@@ -1,13 +1,14 @@
 :80, :443 {
+    import access_log default
+    import compression
+    import security_headers
+
     tls internal
 
     root * /var/www/vito-splash
     file_server
 
     header {
-        X-Content-Type-Options "nosniff"
-        X-Frame-Options "DENY"
-        Referrer-Policy "strict-origin-when-cross-origin"
         Cache-Control "public, max-age=3600"
     }
 }
