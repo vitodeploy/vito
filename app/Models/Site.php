9aadd2af78a249d46bf907f5b572b74ba2df4620
Richard Anderson <richard@andermail.co.uk>
2026-05-25 12:39:27 +0100

    fix for all site types

--- app/Models/Site.php
+++ app/Models/Site.php
@@ -16,6 +16,7 @@ use App\Jobs\SSL\DeleteSiteSslJob;
 use App\Services\Webserver\Webserver;
 use App\SiteFeatures\ActionInterface;
 use App\SiteTypes\AbstractProxiedSiteType;
+use App\SiteTypes\AbstractSiteType;
 use App\SiteTypes\BunSite;
 use App\SiteTypes\NodeSite;
 use App\SiteTypes\SiteType;
@@ -593,10 +594,15 @@ class Site extends AbstractModel
         $required = [];
 
         foreach ($this->siblingsSharingUser(includeSelf: true)->get() as $site) {
-            $typeId = $site->type()::id();
+            $type = $site->type();
+            if (! $type instanceof AbstractSiteType) {
+                continue;
+            }
+
+            $typeId = $type::id();
             $label = config('site.types.'.$typeId.'.label') ?? $typeId;
 
-            foreach ($site->type()::requiredTooling() as $toolId) {
+            foreach ($type::requiredTooling() as $toolId) {
                 $required[$toolId] = $label;
             }
         }
