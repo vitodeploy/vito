672e175635e423321e68edabb5666789700acf96
Richard Anderson <richard@andermail.co.uk>
2026-07-14 20:00:31 +0100

    backup/restore without owner/privileges

--- resources/views/ssh/services/database/postgresql/restore.blade.php
+++ resources/views/ssh/services/database/postgresql/restore.blade.php
@@ -1,4 +1,4 @@
-if ! bash -c 'set -o pipefail; gunzip -c "$2" | sudo -u postgres psql -d "$1"' _ {!! escapeshellarg($database) !!} {!! escapeshellarg($path) !!}; then
+if ! bash -c 'set -o pipefail; gunzip -c "$2" | sudo -u postgres psql -v ON_ERROR_STOP=1 -d "$1"' _ {!! escapeshellarg($database) !!} {!! escapeshellarg($path) !!}; then
     echo 'VITO_SSH_ERROR' && exit 1
 fi
 
